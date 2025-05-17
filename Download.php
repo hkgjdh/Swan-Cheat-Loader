de95addd691777998c858fa66d4fb076577a8df4
hkgjdh <eonhemant20@gmail.com>
2025-05-17 12:21:36 +0530

    Update Download.php

--- Download.php
+++ Download.php
@@ -1,4 +1,4 @@
 {
   "version": "0.01",
-  "url": "https://raw.githubusercontent.com/hkgjdh/HK-CHEATS-LOADER-V2/main/libHKCHEATS.so"
+  "url": "https://raw.githubusercontent.com/hkgjdh/Swan-Cheat-Loader/main/libHKCHEATS.so"
 }
